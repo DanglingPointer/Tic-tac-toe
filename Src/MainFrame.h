1052af18102e7a262798e0cedc08c1af8f5be991
Mikhail Vasilyev <mikail.vasilyev@gmail.com>
2015-08-18 12:59:28 +0200

    GUI core added

--- Src/MainFrame.h
+++ Src/MainFrame.h
@@ -1,50 +1,78 @@
 #pragma once
 #include<afxwin.h>
+#include<sstream>
+#include<string>
+#include"Tictactoe.h"
+using namespace ttt;
 
 class CMainFrame :public CFrameWnd
 {
 public:
 	CMainFrame();
 	afx_msg void OnPaint();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint pt);
 	DECLARE_MESSAGE_MAP();
 private:
-	int m_cx;
-	int m_cy;
+	CRect m_wr, m_crosses_button, m_noughts_button;
+	int m_wr_cx, m_wr_cy;
+	Mark m_side;
 };
 
-CMainFrame::CMainFrame() :m_cx(106*4), m_cy(106*4)
+CMainFrame::CMainFrame() :m_side(Mark::empty)
 {
-	Create(NULL, L"Malevich's Black Square", WS_OVERLAPPEDWINDOW, CRect(0, 0, m_cx, m_cy));
+	Create(NULL, L"Tic-tac-toe", WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_SIZEBOX, CRect(0, 0, 500, 530));
+
+	GetClientRect(m_wr);
+	m_wr.top += 50;
+	m_wr.left += 50;
+	m_wr.bottom -= 80;
+	m_wr.right -= 50;
+	m_wr.OffsetRect(0, 40);
+	m_wr_cx = m_wr.Size().cx;
+	m_wr_cy = m_wr.Size().cy;
+
+	CClientDC dc(this);
+	m_crosses_button = CRect(m_wr.left, 30, m_wr.left + dc.GetTextExtent("Crosses").cx + 10, 30 + dc.GetTextExtent("Crosses").cy + 10);
+
+	int left = m_wr.left + dc.GetTextExtent("Crosses").cx + 10;
+	m_noughts_button = CRect(left + 30, 30, left + 30 + dc.GetTextExtent("Crosses").cx + 10, 30 + dc.GetTextExtent("Noughts").cy + 10);
+
 	CenterWindow();
 }
 
-void CMainFrame::OnPaint()
+inline void CMainFrame::OnPaint()
 {
 	CPaintDC dc(this);
-	CRect wr;
+	dc.Rectangle(m_wr);
 
-	GetWindowRect(wr);
-	m_cx = wr.Size().cx;
-	m_cy = wr.Size().cy;
-	if (m_cx != m_cy)
-	{
-		int size = m_cx > m_cy ? m_cx : m_cy;
-		SetWindowPos(NULL, 0, 0, size, size, SWP_NOMOVE);
-	}
+	CRect vrect(m_wr.left + m_wr_cx / 3, m_wr.top, m_wr.right - m_wr_cx / 3, m_wr.bottom);
+	dc.Rectangle(vrect);
+	
+	dc.SelectStockObject(HOLLOW_BRUSH);
+	CRect hrect(m_wr.left, m_wr.top + m_wr_cy / 3, m_wr.right, m_wr.bottom - m_wr_cy / 3);
+	dc.Rectangle(hrect);
 
-	// --The Black Square--
-	GetClientRect(wr);
-	m_cx = wr.Size().cx;
-	m_cy = wr.Size().cy;
-	int min_dim = m_cx < m_cy ? m_cx : m_cy;
-	int side_length = min_dim * 80.0 / 106.0;
-	int center_x = m_cx / 2, center_y = m_cy / 2;
-	dc.SelectStockObject(BLACK_BRUSH);
-	dc.Rectangle(CRect(center_x - side_length/2, center_y - side_length/2, 
-					   center_x + side_length/2, center_y + side_length/2));
-	// --------------------
+	(m_side == Mark::cross) ? dc.SelectStockObject(GRAY_BRUSH) : dc.SelectStockObject(HOLLOW_BRUSH);
+	dc.Rectangle(m_crosses_button);
+	dc.DrawTextW("Crosses", m_crosses_button, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+	m_side == (Mark::nought) ? dc.SelectStockObject(GRAY_BRUSH) : dc.SelectStockObject(HOLLOW_BRUSH);
+	dc.Rectangle(m_noughts_button);
+	dc.DrawTextW("Noughts", m_noughts_button, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+}
+
+inline void CMainFrame::OnLButtonDown(UINT nFlags, CPoint pt)
+{
+	// Choosing side
+	if (m_side == Mark::empty && m_crosses_button.PtInRect(pt))
+		m_side = Mark::cross;
+	else if (m_side == Mark::empty && m_noughts_button.PtInRect(pt))
+		m_side = Mark::nought;
+
+	Invalidate(TRUE);
 }
 
 BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
 	ON_WM_PAINT()
+	ON_WM_LBUTTONDOWN()
 END_MESSAGE_MAP()
--- Src/Tictactoe.h
+++ Src/Tictactoe.h
@@ -1,1 +1,173 @@
 #pragma once
+#include<stdexcept>
+#include<utility>
+
+#define ROW first
+#define COL second
+
+namespace ttt
+{
+	typedef unsigned int uint;
+	typedef std::pair<uint, uint> Node;
+
+	enum class Mark
+	{
+		empty,
+		cross,
+		nought
+	};
+	enum class Result
+	{
+		win,
+		loss,
+		draw
+	};
+	class Grid
+	{
+	public:
+		Grid() :m_pdata(new Mark[9])
+		{
+			for (uint i = 0; i < 9; ++i)
+				*(m_pdata + i) = Mark::empty;
+		}
+		Grid(const Grid& g) :Grid()
+		{
+			operator=(g);
+		}
+		Grid& operator = (const Grid& g)
+		{
+			for (uint i = 0; i < 9; ++i)
+				*(m_pdata + i) = *(g.m_pdata + i);
+			return *this;
+		}
+		~Grid()
+		{
+			delete[] m_pdata;
+		}
+		Mark at(uint row, uint col) const
+		{
+			return *(m_pdata + row * 3 + col);
+		}
+		bool SetCross(uint row, uint col)
+		{
+			if (at(row, col) == Mark::empty)
+			{
+				*(m_pdata + row * 3 + col) = Mark::cross;
+				return true;
+			}
+			return false;
+		}
+		bool SetNought(uint row, uint col)
+		{
+			if (at(row, col) == Mark::empty)
+			{
+				*(m_pdata + row * 3 + col) = Mark::nought;
+				return true;
+			}
+			return false;
+		}
+		Mark Won() const
+		{
+			bool all_cross = true, all_nought = true;
+			for (uint row = 0; row < 3; ++row)
+			{
+				for (uint col = 0; (col < 3); ++col)
+					won_at(row, col, all_cross, all_nought);
+				if (all_cross)
+					return Mark::cross;
+				else if (all_nought)
+					return Mark::nought;
+			}
+
+			all_cross = all_nought = true;
+			for (uint col = 0; col < 3; ++col)
+			{
+				for (uint row = 0; (row < 3); ++row)
+					won_at(row, col, all_cross, all_nought);
+				if (all_cross)
+					return Mark::cross;
+				else if (all_nought)
+					return Mark::nought;
+			}
+
+			all_cross = all_nought = true;
+			for (uint row = 0, col = 0; row < 3; row = (++col))
+				won_at(row, col, all_cross, all_nought);
+			if (all_cross)
+				return Mark::cross;
+			else if (all_nought)
+				return Mark::nought;
+
+			all_cross = all_nought = true;
+			for (uint row = 2, col = 0; col < 3; row = 2 - (++col))
+				won_at(row, col, all_cross, all_nought);
+			if (all_cross)
+				return Mark::cross;
+			else if (all_nought)
+				return Mark::nought;
+
+			return Mark::empty;
+		}
+	private:
+		Mark *m_pdata;
+		void won_at(uint row, uint col, bool& all_cross, bool& all_nought) const
+		{
+			Mark temp = at(row, col);
+			if (temp == Mark::empty)
+				all_cross = all_nought = false;
+			else if (temp == Mark::cross)
+				all_nought = false;
+			else /*if (temp == Mark::nought) */
+				all_cross = false;
+		}
+	};
+	class Ttt
+	{
+	public:
+		Ttt() :pgrid(new Grid())
+		{
+			;
+		}
+		~Ttt()
+		{
+			if (pgrid != nullptr)
+				delete pgrid;
+		}
+		void Play()
+		{ }
+		Grid* pgrid;
+	private:
+		Result Minimax(Grid*& pg, Node& node, Mark turn, Mark side)
+		{
+			if (turn == Mark::empty || side == Mark::empty)
+				throw std::invalid_argument("Ttt::Minimax()");
+
+			Grid* pnewg = new Grid(*pg);
+			for (uint row = 0; row < 3; ++row)
+				for (uint col = 0; col < 3; ++col)
+				{
+					if (pg->at(row, col) == Mark::empty)
+					{	// Changing state
+						(turn == Mark::cross) ? pnewg->SetCross(row, col) : pnewg->SetNought(row, col);
+					}
+					Mark winner = pnewg->Won();
+					if ((winner == Mark::cross && side == Mark::cross) ||
+						(winner == Mark::nought && side == Mark::nought))
+					{	// Terminal node
+						delete pg;
+						pg = pnewg;
+						return Result::win;
+					}
+					if ((winner == Mark::cross && side == Mark::nought) ||
+						(winner == Mark::nought && side == Mark::cross))
+					{	// Terminal node
+						delete pnewg;
+						return Result::loss;
+					}
+					// Intermediate node
+					Result temp = Minimax(pnewg, node, ((turn == Mark::cross) ? Mark::nought : Mark::cross), side);
+
+				}
+		}
+	};
+}
